b218f172f0299695439c4d98497a2166f71391ef
Marshall Nguyen <marshahn@uci.edu>
2020-02-06 15:56:59 -0800

    Generate basic course

---
+++ easy_course.py
@@ -0,0 +1,159 @@
+from __future__ import print_function
+from __future__ import division
+# ------------------------------------------------------------------------------------------------
+# Copyright (c) 2016 Microsoft Corporation
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+# associated documentation files (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge, publish, distribute,
+# sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+# NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# ------------------------------------------------------------------------------------------------
+
+# Tutorial sample #4: Challenge - get to the centre of the sponge
+
+from builtins import range
+from past.utils import old_div
+import MalmoPython
+import os
+import sys
+import time
+
+if sys.version_info[0] == 2:
+    sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
+else:
+    import functools
+    print = functools.partial(print, flush=True)
+
+
+
+def gen_simple_course_one():
+    
+    path_blocks = ''''''
+    for i in range(1,11):
+        path_blocks += '''<DrawBlock x="''' + str(i) + '''" y="1" z="0" type="dark_oak_planks"/>\n'''
+    decorate ='''
+    <DrawingDecorator>
+        <FloorBlock type="lava"/>
+        <DrawBlock x="0" y="1" z="0" type="dark_oak_plank"/>\n
+        {}
+    </DrawingDecorator>
+    '''
+    return decorate.format(path_blocks)
+'''
+def gen_random_simple(length, width):
+
+"""
+Generates a basic rectangular course with given length and width.
+Returns Drawing decorator string to be appended to appropriate
+XML mission section.
+"""
+	return None
+'''      
+
+missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+            <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            
+              <About>
+                <Summary>Hello world!</Summary>
+              </About>
+              
+            <ServerSection>
+              <ServerInitialConditions>
+                <Time>
+                    <StartTime>1000</StartTime>
+                    <AllowPassageOfTime>false</AllowPassageOfTime>
+                </Time>
+                <Weather>clear</Weather>
+              </ServerInitialConditions>
+              <ServerHandlers>
+              	<FlatWorldGenerator generatorString="3;7,44*49,73,35:1,159:4,95:13,35:13,159:11,95:10,159:14,159:6,35:6,95:6;12;"/>
+            '''+ gen_simple_course_one() +'''
+                  <ServerQuitFromTimeUp timeLimitMs="30000"/>
+                  <ServerQuitWhenAnyAgentFinishes/>
+                </ServerHandlers>
+              </ServerSection>
+              
+              <AgentSection mode="Survival">
+                <Name>MalmoTutorialBot</Name>
+                <AgentStart>
+                    <Placement x="0" y="2" z="0" yaw="0"/>
+                </AgentStart>
+                <AgentHandlers>
+                  <ObservationFromFullStats/>
+                  <ContinuousMovementCommands turnSpeedDegs="180"/>
+                  <InventoryCommands/>
+                  <AgentQuitFromReachingPosition>
+                    <Marker x="-26.5" y="40" z="0.5" tolerance="0.5" description="Goal_found"/>
+                  </AgentQuitFromReachingPosition>
+                </AgentHandlers>
+              </AgentSection>
+            </Mission>'''
+
+# Create default Malmo objects:
+
+agent_host = MalmoPython.AgentHost()
+try:
+    agent_host.parse( sys.argv )
+except RuntimeError as e:
+    print('ERROR:',e)
+    print(agent_host.getUsage())
+    exit(1)
+if agent_host.receivedArgument("help"):
+    print(agent_host.getUsage())
+    exit(0)
+
+my_mission = MalmoPython.MissionSpec(missionXML, True)
+my_mission_record = MalmoPython.MissionRecordSpec()
+
+# Attempt to start a mission:
+max_retries = 3
+for retry in range(max_retries):
+    try:
+        agent_host.startMission( my_mission, my_mission_record )
+        break
+    except RuntimeError as e:
+        if retry == max_retries - 1:
+            print("Error starting mission:",e)
+            exit(1)
+        else:
+            time.sleep(2)
+
+# Loop until mission starts:
+print("Waiting for the mission to start ", end=' ')
+world_state = agent_host.getWorldState()
+while not world_state.has_mission_begun:
+    print(".", end="")
+    time.sleep(0.1)
+    world_state = agent_host.getWorldState()
+    for error in world_state.errors:
+        print("Error:",error.text)
+
+print()
+print("Mission running ", end=' ')
+
+# ADD YOUR CODE HERE
+time.sleep(2)
+agent_host.sendCommand("move 1")
+
+
+# Loop until mission ends:
+while world_state.is_mission_running:
+    print(".", end="")
+    time.sleep(0.1)
+    world_state = agent_host.getWorldState()
+    for error in world_state.errors:
+        print("Error:",error.text)
+
+print()
+print("Mission ended")
+# Mission has ended.
